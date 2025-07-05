77ed9631f204f60326188e5b9aba1fd5702f911f
Julio Paredes <pabel_89@hotmail.com>
2025-07-04 21:28:33 -0500

    💄 Formatted hours in weekly PDF report

--- registros/pdf.py
+++ registros/pdf.py
@@ -4,6 +4,7 @@ from django.template.loader import get_template
 from xhtml2pdf import pisa
 from .models import Registro
 from django.db.models import Sum
+from decimal import Decimal
 
 def generar_pdf_reporte_semanal(fecha_inicio, fecha_fin):
     # ✅ Convertir strings a objetos date
@@ -33,3 +34,29 @@ def generar_pdf_reporte_semanal(fecha_inicio, fecha_fin):
     if pisa_status.err:
         return HttpResponse('Error al generar PDF', status=500)
     return response
+
+
+
+def formatear_decimal_a_horas(decimal_horas):
+    if not decimal_horas:
+        return "0h 0min"
+    total_min = int(decimal_horas * 60)
+    horas = total_min // 60
+    minutos = total_min % 60
+    return f"{horas}h {minutos}min"
+
+resumen = registros.values('nombre_empleado').annotate(total_horas=Sum('total_horas'))
+
+# Armar estructura con formato para el template
+resumen_formateado = {
+    r['nombre_empleado']: {
+        'raw': r['total_horas'],
+        'formateado': formatear_decimal_a_horas(r['total_horas']),
+        'estado': (
+            "Incompleto" if r['total_horas'] < Decimal(40) else
+            "Exceso" if r['total_horas'] > Decimal(40) else
+            "Completo"
+        )
+    }
+    for r in resumen
+}
